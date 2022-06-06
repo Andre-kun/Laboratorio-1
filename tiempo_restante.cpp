68dfe74fc2b1f22fe2c2b859d18965986d272c58
André-kun <asalinasc10@gmail.com>
2022-06-05 23:33:37 -0500

    Esta era la plantilla xd

---
+++ tiempo_restante.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+  // Especificaciones de entrada
+
+  // Especificaciones de salida
+
+  // Ingreso de datos
+  
+  // Proceso
+  
+  // Salida de resultados
+}
